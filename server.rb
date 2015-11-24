68b2225b79b12f504fd95de302c895b168676a2a
Adam Pash <adam.pash@gmail.com>
2015-11-24 16:37:11 -0500

    Using sinatra reloader instead of rerun

--- server.rb
+++ server.rb
@@ -1,6 +1,15 @@
 require 'sinatra'
+require 'sinatra/reloader'
 require_relative './app'
-configure { set :server, :puma }
+
+def development?
+  ENV["ENVIRONMENT"] == 'development'
+end
+configure do
+  set :server, :puma
+  enable :reloader if development?
+  also_reload '*.rb'
+end
 
 get '/wiki/:title' do |title|
   article = Article.find_by(title: title.gsub('_', ' '))
